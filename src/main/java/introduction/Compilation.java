0528a0ae2dd2b04887a85ae5832407561f18cbd8
philzhuqigong <philzhuqigong@gmail.com>
2025-01-15 21:11:32 -0500

    cs61B 1 introduction

---
+++ src/main/java/introduction/Compilation.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class Compilation {
+    public static void main(String[] args) {
+
+    }
+}
+/**
+ * Why make a class file at all
+ *  .class file has been type checked. Distributed code is safer.
+ *  .class files are 'simpler' for machine to execute. Distributed code is faster.
+ *  Minor benefit:Protects your intellectual property.No need to give out source.
+ */
---
+++ src/main/java/introduction/Introduction.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
+
+/**
+ * 1.In Java, all code must be part of a class.
+ * 2.Classes are defined with "public class CLASSNAME"
+ * 3.We use {} to delineate the beginning and ending of things
+ * 4.We must end lines with a semicolon.
+ * 5.The code we want to run must be under static void main ...
+ */
